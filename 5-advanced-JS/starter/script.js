c49de74fe8d9bb278972f8ab0515ed0352b8f654
Simon Walker <simon.walker@meritec-dc.local>
2019-06-13 16:17:23 +0100

    20190613W

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -74,12 +74,76 @@
 
 // Primatives vs Objects
 
-var a =23;
-var b = a;
-a = 46; 
-console.log(a);
-console.log(b);
+// var a =23;
+// var b = a;
+// a = 46; 
+// console.log(a);
+// console.log(b);
 
+
+// var years = [1990, 1965, 1937, 2005, 1998];
+
+// function arrayCalc(arr, fn) {
+//     var arrRes = [];
+//     for (var i = 0; i < arr.length; i++) {
+//         arrRes.push(fn(arr[i]));
+//     }
+// return arrRes;
+// }
+
+// function calcAge(el) {
+//     return 2019 - el;
+// }
+
+
+// function isFullAge(el) {
+//     return el <= 18;
+// }
+
+// function maxHeartRate(el) {
+//     if(el >= 18 && el <=81) {
+//     return Math.round(286.9 - (0.67 * el));    
+//     } else {
+//         return -1;
+//     }
+    
+// }
+
+
+// var ages = arrayCalc(years, calcAge);
+// var fullAges =arrayCalc(ages, isFullAge);
+// var heartRate = arrayCalc(ages, maxHeartRate);
+
+// console.log(ages);
+// console.log(fullAges);
+// console.log(heartRate);
+
+// Functions returning Functions
+
+function interviewQuestions(job) {
+    if (job === 'designer') {
+        return function(name) {
+            console.log(name + ', can you please explin what UX design is?');
+        }
+    } else if (job === 'teacher') {
+        return function(name) {
+            console.log('What subject do you teach, ' + name + '?'); 
+    }
+} else {
+    return function(name) {
+        console.log(name + ' What the fuck are you doing here?');
+    }
+}
+}
+
+var teacherQuestion = interviewQuestions('teacher');
+var engineerQuestion = interviewQuestions('engineer')
+
+engineerQuestion('john');
+
+teacherQuestion('john');
+
+interviewQuestions('teacher')('fred');
 
 
 
